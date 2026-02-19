211485c2035c165a8de43268f62e9c4df9621eea
[krs7000] <[disasters0022@gmail.coml>
2026-02-19 08:07:02 +0800

    fixing some errors

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,26 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render
 
-# Create your views here.
+from .models import College, Program, Organization, Student, OrgMember
+
+
+def home(request):
+    return render(request, "studentorg/home.html")
+
+
+def college_list(request):
+    q = request.GET.get("q", "").strip()
+    qs = College.objects.all().order_by("college_name")
+
+    if q:
+        qs = qs.filter(college_name__icontains=q)
+
+    paginator = Paginator(qs, 10)  # 10 per page
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "studentorg/college_list.html", {
+        "page_obj": page_obj,
+        "q": q,
+    })
