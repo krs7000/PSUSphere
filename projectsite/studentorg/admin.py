fb775dff1ae90279e319daf1b38c84e5032f3cd4
[krs7000] <[disasters0022@gmail.coml>
2026-02-23 09:49:53 +0800

    fixing the ui buttons

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,21 +1,8 @@
-from django.contrib import admin
+﻿from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
-
-
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
-    list_filter = ("program",)
-    search_fields = ("student_id", "lastname", "firstname")
-
-
-@admin.register(OrgMember)
-class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ("student", "organization", "date_joined")
-    list_filter = ("organization",)
-    search_fields = ("student__lastname", "student__firstname", "organization__name")
-
 
 admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
+admin.site.register(Student)
+admin.site.register(OrgMember)
